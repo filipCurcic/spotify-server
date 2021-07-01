fbe9b9d3e077b5a75099944bc77541a67699cc24
Filip Curcic <filipcurcic42@gmail.com>
2021-07-01 17:44:10 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '04e18db5ed194896aa99974f61e42935',
+    clientSecret: 'dddaa414fa7d4a539078b9232420ee25',
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
+app.post('/login', (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '04e18db5ed194896aa99974f61e42935',
+    clientSecret: 'dddaa414fa7d4a539078b9232420ee25',
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refreshToken,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
